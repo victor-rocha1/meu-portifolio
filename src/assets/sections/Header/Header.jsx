67c58aec43987bef4c8f0a57e947e1054c7e19c3
Victor Rocha <145049490+victor-rocha1@users.noreply.github.com>
2024-04-10 16:16:25 -0300

    Começo do Header

--- src/assets/sections/Header/Header.jsx
+++ src/assets/sections/Header/Header.jsx
@@ -1,12 +1,28 @@
-
+import './header.css';
+import '../../../../src/index.css';
 function Header() {
-    return(
+    return(    
       <header>
         <div className="interface">
             <div className="logo">
                 <a href="">
-                    <img src="./public/images/logo.png" alt="Logo Victor Rocha" />
+                    <img id="logotipo" src="./public/images/logo.png" alt="Logo Victor Rocha" />
                 </a>
+            </div>
+
+            <nav className="menu-desktop">
+              <ul>
+                <li><a href="#">Início</a></li>
+                <li><a href="#">Especialidades</a></li>
+                <li><a href="#">Sobre</a></li>
+                <li><a href="#">Projetos</a></li>
+              </ul>
+            </nav>
+
+            <div className="btn-contato">
+              <a href="#">
+                <button>Contato</button>
+              </a>
             </div>
         </div>
       </header>
