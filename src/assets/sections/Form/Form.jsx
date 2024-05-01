bf4dbb4d93dc4ddb32b6abc6811469c005424d7c
Victor Rocha <145049490+victor-rocha1@users.noreply.github.com>
2024-05-01 13:42:41 -0300

    Validação do Formulário

--- src/assets/sections/Form/Form.jsx
+++ src/assets/sections/Form/Form.jsx
@@ -1,19 +1,40 @@
-import Submit from "../../components/Button/Submit/Submit";
+import React, { useState } from 'react';
+import Submit from "../../components/Button/Submit/Submit.jsx";
 import Title from "../../components/Title/Title";
+import PhoneInput from 'react-phone-number-input'
 import "./form.css";
 
-function Form(){
-    return(
+function Form() {
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const [email, setEmail] = useState("");
+    const [message, setMessage] = useState("");
+    const [emailError, setEmailError] = useState(false);
+
+    const handleEmailChange = (event) => {
+        const email = event.target.value;
+        const isValid = emailRegex.test(email);
+        setEmail(email);
+        setEmailError(!isValid);
+    }
+
+    const handleMessageChange = (event) => {
+        setMessage(event.target.value);
+    }
+
+    return (
         <section className="form">
             <div className="interface">
-                <Title text="FALE" spantext="COMIGO"/>
+                <Title text="FALE" spantext="COMIGO" />
 
-                <form action="">
-                    <input type="text" placeholder="Seu nome:" required/>
-                    <input type="email" placeholder="Seu email:" required/>
+                <form onSubmit={(e) => e.preventDefault()}>
+                    <input type="text" id="name" placeholder="Seu nome:" required />
+                    <input type="email" id="email" placeholder="Seu email:" value={email} onChange={handleEmailChange} required className={emailError ? 'invalid' : ''} />
+                    <span className="error-message">{emailError && "Por favor, insira um email válido"}</span>
+
                     <input type="tel" placeholder="Seu número de telefone (opcional):" />
-                    <textarea placeholder="Sua mensagem:" required></textarea>
-                    <Submit text="ENVIAR" form="myForm" />
+                    <textarea placeholder="Sua mensagem:" value={message} onChange={handleMessageChange} required></textarea>
+                    <Submit text="ENVIAR" />
                 </form>
             </div>
         </section>
