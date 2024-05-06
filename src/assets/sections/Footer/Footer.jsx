29bcbd3bbff2fa7044762b844ff569b46efaaaca
Victor Rocha <145049490+victor-rocha1@users.noreply.github.com>
2024-05-06 17:09:37 -0300

    Footer

--- src/assets/sections/Footer/Footer.jsx
+++ src/assets/sections/Footer/Footer.jsx
@@ -1,21 +1,28 @@
 import Icons from '../../components/Icons/Icons';
 import './footer.css'
 
-function Footer(){
+function Footer() {
     const icons = ['instagram', 'github', 'linkedin', 'envelope-fill'];
-    return(
+    return (
         <footer>
             <div className="interface">
                 <div className="line-footer">
-                    <div className="logo-footer">
-                        <img id='logotipo' src="./public/images/logo.png" alt="Logo Victor Rocha" />
+                    <div className="flex">
+                        <div className="logo-footer">
+                            <img id='logotipo' src="./public/images/logo.png" alt="Logo Victor Rocha" />
+                        </div>
+                        <Icons icons={[icons[0], icons[1], icons[2]]} size="2rem" alignment="left" />
                     </div>
-                    <Icons icons={[icons[0], icons[1], icons[2]]} size="2.5rem" alignment="left" />
                 </div>
 
+                <span className="line-separator"></span>
                 <div className="line-footer">
-                    <p> <Icons icons={[icons[3]]} size="2.5rem" alignment="left"/> <a href="mailto:victorrocha3202@gmail">victorrocha3202@gmail</a></p>
-                
+                    <p className='line'>
+                        <div className="icon-container">
+                            <Icons icons={[icons[3]]} size="2.5rem" alignment="left" />
+                            <a href="mailto:victorrocha3202@gmail">victorrocha3202@gmail</a>
+                        </div>
+                    </p>
                 </div>
             </div>
         </footer>
