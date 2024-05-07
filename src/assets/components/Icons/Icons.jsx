5ffe3a211dc9d6cf3b5de5df1748b7ce73d3f97d
Victor Rocha <145049490+victor-rocha1@users.noreply.github.com>
2024-05-07 16:27:15 -0300

    Responsividade Mobile

--- src/assets/components/Icons/Icons.jsx
+++ src/assets/components/Icons/Icons.jsx
@@ -5,7 +5,15 @@ function Icons({ icons = [], size = '2rem', alignment = 'flex-end' }) {
     return (
         <div className="icon-social" style={{ justifyContent: alignment }}>
             {icons && Array.isArray(icons) && icons.map((icon, index) => (
-                <a key={index} href="#"><i className={`bi bi-${icon}`} style={{ fontSize: size, color: 'var(--blue)' }}></i></a>
+                <a key={index} href="#">
+                    <i
+                        className={`bi bi-${icon}`}
+                        style={{
+                            fontSize: size,
+                            color: 'var(--blue)'
+                        }}
+                    />
+                </a>
             ))}
         </div>
     );
--- src/assets/sections/About/About.jsx
+++ src/assets/sections/About/About.jsx
@@ -13,13 +13,13 @@ function About() {
                     </div>
 
                     <div className="txt-about">
-                        <Title text="PRAZER, SOU" spantext=" VICTOR ROCHA."/>
+                        <Title text="PRAZER, SOU" spantext=" VICTOR ROCHA." />
 
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat possimus distinctio ex tenetur, blanditiis temporibus cupiditate autem, rem pariatur at iste facere incidunt officia alias recusandae deleniti aperiam provident. Totam!</p>
 
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea sit unde saepe repudiandae vitae tempora. Nostrum nulla soluta a eligendi, laboriosam illo accusamus, quo dolores neque iste inventore vel sit!</p>
 
-                        <Icons icons={socialIcons} /> 
+                        <Icons icons={socialIcons} className="small-icons" />
                     </div>
                 </div>
             </div>
