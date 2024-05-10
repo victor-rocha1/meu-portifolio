4381eb6e26c7b71f80fc94b09aba56d0755117de
Victor Rocha <145049490+victor-rocha1@users.noreply.github.com>
2024-05-10 17:55:10 -0300

    Criação do Menu para Mobile

--- src/assets/sections/Header/Header.jsx
+++ src/assets/sections/Header/Header.jsx
@@ -1,15 +1,26 @@
-import "./header.css";
-import '../../../../src/index.css';
+import React, { useState } from 'react';
+import './header.css';
+import logo from '/public/images/logo.png';
 import Button from '../../components/Button/Button/Button.jsx';
-import logo from '/public/images/logo.png'
-
 
 function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+
+    if (isMenuOpen) {
+      setTimeout(() => {
+        setIsMenuOpen(false);
+      }, 500);
+    }
+  };
+
   return (
     <header>
       <div className="interface">
         <div className="logo">
-          <a href="">
+          <a href="/">
             <img id="logotipo" src={logo} alt="Logo" />
           </a>
         </div>
@@ -23,10 +34,32 @@ function Header() {
         </nav>
 
         <Button>Contato</Button>
+
+        <div className="btn-open-menu" onClick={toggleMenu}>
+          <i className="bi bi-list"></i>
+        </div>
+
+        {isMenuOpen && (
+          <div className="menu-mobile open">
+            <div className="btn-close" onClick={toggleMenu}>
+              <i className="bi bi-x-lg"></i>
+            </div>
+
+            <nav>
+              <ul>
+                <li><a href="#">Início</a></li>
+                <li><a href="#">Especialidades</a></li>
+                <li><a href="#">Projetos</a></li>
+                <li><a href="#">Contato</a></li>
+              </ul>
+            </nav>
+          </div>
+        )}
+
+        {isMenuOpen && <div className="overlay-menu open" onClick={toggleMenu}></div>}
       </div>
     </header>
-  )
-
+  );
 }
 
-export default Header
+export default Header;
