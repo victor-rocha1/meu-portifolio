cc4627ee8719e0f0a1dd22379d5a448fb4b6cc66
Victor Rocha <145049490+victor-rocha1@users.noreply.github.com>
2024-05-11 16:50:30 -0300

    Update About

--- src/assets/components/Icons/Icons.jsx
+++ src/assets/components/Icons/Icons.jsx
@@ -2,21 +2,21 @@ import React from 'react';
 import './icons.css';
 
 function Icons({ icons = [], size = '2rem', alignment = 'flex-end' }) {
-    return (
-        <div className="icon-social" style={{ justifyContent: alignment }}>
-            {icons && Array.isArray(icons) && icons.map((icon, index) => (
-                <a key={index} href="#">
-                    <i
-                        className={`bi bi-${icon}`}
-                        style={{
-                            fontSize: size,
-                            color: 'var(--blue)'
-                        }}
-                    />
-                </a>
-            ))}
-        </div>
-    );
+  return (
+    <div className="icon-social" style={{ justifyContent: alignment }}>
+      {icons && Array.isArray(icons) && icons.map((iconObj, index) => (
+        <a key={index} href={iconObj.link} target='_blank'>
+          <i
+            className={`bi bi-${iconObj.name}`}
+            style={{
+              fontSize: size,
+              color: 'var(--blue)'
+            }}
+          />
+        </a>
+      ))}
+    </div>
+  );
 }
 
 export default Icons;
--- src/assets/sections/About/About.jsx
+++ src/assets/sections/About/About.jsx
@@ -5,27 +5,34 @@ import avatar from '/public/images/avatar.jpg';
 import './about.css';
 
 function About() {
-    const socialIcons = ['instagram', 'github', 'linkedin'];
-    return (
-        <section className='about'>
-            <div className="interface">
-                <div className="flex">
-                    <div className="img-about">
-                        <img id='img-txt' src={avatar} alt="avatar" />
-                    </div>
+  const socialIcons = [
+    { name: 'instagram', link: 'https://www.instagram.com/victor_rocha924/?next=%2F' },
+    { name: 'github', link: 'https://www.github.com/victor-rocha1' },
+    { name: 'linkedin', link: 'https://www.linkedin.com/in/victor-rocha-dos-reis-751376267/' },
+  ];
+  return (
+    <section className='about'>
+      <div className="interface">
+        <div className="flex">
+          <div className="img-about">
+            <img id='img-txt' src={avatar} alt="avatar" />
+          </div>
 
-                    <div className="content">
-                        <Title text="PRAZER, SOU" spantext=" VICTOR ROCHA." />
-                        <div className="txt-about">
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat possimus distinctio ex tenetur, blanditiis temporibus cupiditate autem, rem pariatur at iste facere incidunt officia alias recusandae deleniti aperiam provident. Totam!</p>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea sit unde saepe repudiandae vitae tempora. Nostrum nulla soluta a eligendi, laboriosam illo accusamus, quo dolores neque iste inventore vel sit!</p>
-                        </div>
-                        <Icons icons={socialIcons} className="small-icons" />
-                    </div>
-                </div>
+          <div className="content">
+            <Title text="PRAZER, SOU" spantext=" VICTOR ROCHA." />
+            <div className="txt-about">
+              <p>Sou estudante de <span className='blue'>Análise e Desenvolvimento de Sistemas</span>, apaixonado por tecnologia e programação. Atualmente, estou focando meus estudos no desenvolvimento Web, tanto no Front End quanto no Back End. Busco aprender e aprimorar minhas habilidades para construir soluções inovadoras e funcionais.</p>
+              <p>Participo de diversos cursos que vão além do conhecimento passado na faculdade, onde o foco atual vem sendo o Front-End, em tecnologias como <span className='blue'>HTML5</span>,<span className='blue'> CSS3</span> usando a biblioteca <span className='blue'>React</span> do JavaScript para praticar meus conhecimentos</p>
             </div>
-        </section>
-    )
+            <Icons
+              icons={socialIcons}
+              className="small-icons"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default About;
