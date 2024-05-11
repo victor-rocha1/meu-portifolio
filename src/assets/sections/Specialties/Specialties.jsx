efa2f931f587c8d7e71b0ef20e3bc67c1ae685dd
Victor Rocha <145049490+victor-rocha1@users.noreply.github.com>
2024-05-11 13:31:36 -0300

    Update Specialities

--- src/assets/sections/Specialties/Specialties.jsx
+++ src/assets/sections/Specialties/Specialties.jsx
@@ -11,19 +11,19 @@ function Specialties() {
                         <div className="box">
                             <i class="bi bi-code-square"></i>
                             <h2>FRONT END</h2>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae fugiat perspiciatis nihil quis alias magnam. Nesciunt deserunt reprehenderit ducimus numquam!</p>
+                            <p>Desenvolvo aplicações usando JavaScript como base, HTML5 e CSS3 para marcação, e a Biblioteca <span>React </span> para uma programação mais organizada e eficiente..</p>
                         </div>
 
                         <div className="box">
                             <i class="bi bi-laptop"></i>
                             <h2>BACK END</h2>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae fugiat perspiciatis nihil quis alias magnam. Nesciunt deserunt reprehenderit ducimus numquam!</p>
+                            <p>Tenho experiência acadêmica com a <span>Linguagem Python</span>, cusrsando disciplinas e desenvolvendo aplicações simples, como uma calculadora feita inteiramente com Python</p>
                         </div>
 
                         <div className="box">
                             <i class="bi bi-database"></i>
                             <h2>DADOS</h2>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae fugiat perspiciatis nihil quis alias magnam. Nesciunt deserunt reprehenderit ducimus numquam!</p>
+                            <p>Conhecimentos na <span>Linguagem SQL e e em Banco de Dados Relacional</span> usando o Software <span>PostgreSQL</span>, usando comandos simples para consultas em Tabelas Relacionais</p>
                         </div>
 
                     </div>
