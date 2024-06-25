aa29facc800f5e67c2afb9f8daaa38094ec74cc5
Victor Rocha <145049490+victor-rocha1@users.noreply.github.com>
2024-06-25 17:34:47 -0300

    Responsividade Footer
    Responsividade do footer e ajuste no btn open menu

--- src/assets/sections/Footer/Footer.jsx
+++ src/assets/sections/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 import Icons from '../../components/Icons/Icons';
-import logo from '/public/images/logo.png'
-import './footer.css'
+import logo from '/public/images/logo.png';
+import './footer.css';
 
 function Footer() {
     const socialIcons = [
@@ -9,6 +9,7 @@ function Footer() {
         { name: 'linkedin', link: 'https://www.linkedin.com/in/victor-rocha-dos-reis-751376267/' },
     ];
     const contactIcon = { name: 'envelope-fill', link: 'mailto:victorrocha3202@gmail' };
+
     return (
         <footer>
             <div className="interface">
@@ -22,17 +23,15 @@ function Footer() {
                 </div>
 
                 <span className="line-separator"></span>
-                <div className="line-footer">
-                    <p className='line'>
-                        <div className="icon-container">
-                            <Icons icons={[contactIcon]} size="2.5rem" alignment="left" />
-                            <a href={contactIcon.link}>victorrocha3202@gmail</a>
-                        </div>
-                    </p>
+                <div className="line-footer email-container">
+                    <div className="icon-container">
+                        <Icons icons={[contactIcon]} size="2.5rem" alignment="left" />
+                        <a href={contactIcon.link}>victorrocha3202@gmail</a>
+                    </div>
                 </div>
             </div>
         </footer>
-    )
+    );
 }
 
 export default Footer;
