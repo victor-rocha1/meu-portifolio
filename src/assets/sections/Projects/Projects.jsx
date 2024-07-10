239f22c5b7457d0da47d7bc0b3417f8dd94c68b9
Victor Rocha <145049490+victor-rocha1@users.noreply.github.com>
2024-07-09 21:12:09 -0300

    Atualização dos Projetos exibidos

--- src/assets/sections/Projects/Projects.jsx
+++ src/assets/sections/Projects/Projects.jsx
@@ -4,9 +4,9 @@ import Title from "../../components/Title/Title.jsx";
 import ImgProject from "../../components/ImgProject/ImgProject.jsx";
 import imgSpace from "./images/VRSpaceX.png";
 import imgValidador from "./images/validador.png";
-import imgCalculadoraJs from "./images/calculadorajs.png";
 import imgCalculadoraPy from "./images/calculadorapy.png";
-import imgjogoDaVelha from "./images/jogodavelha.png";
+import imgOneBitFlix from "./images/onebitflix.png"
+import imgJogodeDamas from "./images/jogo-de-dama.png"
 
 
 function Projects() {
@@ -21,25 +21,25 @@ function Projects() {
                         github='https://github.com/victor-rocha1/SpaceX-Bootstrap'
                         button={true}
                     />
+                    
+                    <ImgProject
+                        img={imgJogodeDamas}
+                        text="Jogo de Damas - Projeto Acadêmico"
+                        github='https://github.com/victor-rocha1/jogo-de-damas'
+                        button={true}
+                    />
+
+                    <ImgProject
+                        img={imgOneBitFlix}
+                        text="OneBitFlix"
+                        github='https://github.com/victor-rocha1/onebitflix'
+                        button={true}
+                    />
 
                     <ImgProject
                         img={imgValidador}
                         text="Validador de Email e Senha"
                         github='https://github.com/victor-rocha1/Validador-de-Email-e-Senha'
-                        button={true}
-                    />
-
-                    <ImgProject
-                        img={imgjogoDaVelha}
-                        text="Jogo da Velha"
-                        github='https://github.com/victor-rocha1/Jogo-da-Velha'
-                        button={true}
-                    />
-
-                    <ImgProject
-                        img={imgCalculadoraJs}
-                        text="Calculadora JavaScript"
-                        github='https://github.com/victor-rocha1/Calculadora-JavaScript'
                         button={true}
                     />
 
