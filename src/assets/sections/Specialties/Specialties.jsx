a75eb81604a3f2652eb853f6bc918149785c3202
Victor Rocha <145049490+victor-rocha1@users.noreply.github.com>
2024-10-21 09:15:54 -0300

    .

--- src/assets/sections/Specialties/Specialties.jsx
+++ src/assets/sections/Specialties/Specialties.jsx
@@ -1,7 +1,7 @@
 import Title from "../../components/Title/Title.jsx";
 import "./specialties.css";
 import AOS from 'aos';
-import 'aos/dist/aos.css'; 
+import 'aos/dist/aos.css';
 
 AOS.init();
 
@@ -10,7 +10,7 @@ function Specialties() {
         <section className="specialities" id="specialities">
             <section className="especialidades" >
                 <div className="interface" >
-                    <Title text="MINHAS " spantext="ESPECIALIDADES"/>
+                    <Title text="MINHAS " spantext="ESPECIALIDADES" />
                     <div className="flex">
                         <div className="box">
                             <i class="bi bi-code-square"></i>
@@ -19,10 +19,11 @@ function Specialties() {
                         </div>
 
                         <div className="box">
-                            <i class="bi bi-laptop"></i>
+                            <i className="bi bi-laptop"></i>
                             <h2>BACK END</h2>
-                            <p>Tenho experiência acadêmica com a <span>Linguagem Python</span>, cusrsando disciplinas e desenvolvendo aplicações simples, como uma calculadora feita inteiramente com Python</p>
+                            <p>Possuo experiência acadêmica em <span>Python</span>, com foco na resolução de exercícios e problemas básicos. Além disso, tenho experiência profissional com <span>PHP</span>, utilizando o framework <span>Laravel</span> em projetos na <span>Prodemge</span>.</p>
                         </div>
+
 
                         <div className="box">
                             <i class="bi bi-database"></i>
